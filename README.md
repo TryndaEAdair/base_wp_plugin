a2a58bcf3102a785ee160002049b4339756d8d8c
Trynda E. Adair <tryndaadair@gmail.com>
2025-06-10 20:58:09 -0500

    Committing changes to readme and missing shortcode functionality.

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ Creation Date: 08/06/2025
 Last Update Date: 08/06/2025
 Author: Trynda E. Adair
 Description: This is a base WordPress plugin to save time when creating website plugins.
-Version: 1.0
+Version: 2.0
 
 == Change log ==
 Last Updated: 08/06/2025
 Updated By: Trynda E. Adair
 
 1.0   	- Base Plugin Initialization
+2.0   	- Adding additional directory structure and plugin tests
--- base_custom_plugin.php
+++ base_custom_plugin.php
@@ -4,16 +4,18 @@
 	Creation Date: DD/MM/YYYY
 	Author: [Plugin Author]
 	Description: This is a plugin designed to [Do Something] for the [Domain Name] website.
-	Version: 1.1
+	Version: 1.0
 	
 	Last Updated: [Current Date - DD/MM/YYYY]
 	Updated By: [Update Author]
 */
-	// Define constants
-	define( 'GMD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-	define( 'GMD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 	
-	//Define Variables
+	defined('ABSPATH') || exit;
+	
+	// Define constants
+	define( 'BASIC_CUSTOM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+	define( 'BASIC_CUSTOM_URL', plugin_dir_url( __FILE__ ) );
+   
 	$plugin_content = "Plugin could not be loaded properly";
 
 	function base_custom_install() {
@@ -24,13 +26,32 @@
 		
 	};
 
-	function base_custom_load_files() {
-		wp_register_script( 'base_custom_jquery', plugins_url('/js/base_custom.js', __FILE__), array('jquery'));
+	function register_basic_block() {
+		register_block_type(__DIR__ . '/blocks/basic_block', [
+			'render_callback' => 'render_basic_custom_shortcode'
+		]);
+	}
+	
+	function render_basic_custom_shortcode($atts) {
+		$atts = shortcode_atts([
+			'zoom' => 10,
+			'height' => '400px'
+		], $atts, 'basic_custom_plugin');
+
+		$html = '<div class="basic-shortcode" style="height:' . esc_attr($atts['height']) . ';">';
+		$html .= apply_filters('basic_shortcode_html', '<!-- Something Here -->', $atts);
+		$html .= '</div>';
+
+		return $html;
+	}
+
+	function base_custom_load_admin_files() {
+		wp_register_script( 'base_custom_jquery', plugins_url('/admin/js/base_custom.js', __FILE__), array('jquery'));
 		
 		wp_enqueue_script( 'base_custom_jquery' );
 		
 		
-		wp_register_style( 'base_custom_css', plugins_url('/css/base_custom.css', __FILE__));
+		wp_register_style( 'base_custom_css', plugins_url('/admin/css/base_custom.css', __FILE__));
 		
 		wp_enqueue_style( 'base_custom_css' );
 		
@@ -40,5 +61,10 @@
 	register_activation_hook( __FILE__, 'base_custom_install' );
 	register_deactivation_hook( __FILE__, 'base_custom_deactivate' );
 	
-	add_action('wp_enqueue_scripts', 'base_custom_load_files');	
+	//Init Actions
+	add_action('init', 'register_basic_block');	
+	add_action('wp_enqueue_scripts', 'base_custom_load_admin_files');	
+	
+	//Register Shortcodes
+	add_shortcode('basic_custom_plugin', 'render_basic_custom_shortcode');
 ?>
