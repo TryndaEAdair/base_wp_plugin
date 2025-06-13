df4962a7e708d7f7cbc760271cf3fd23bf137a3a
Trynda E. Adair <tryndaadair@gmail.com>
2025-06-12 20:52:08 -0500

    Adding additional variables to plugin header.
    Signed-off-by: Trynda E. Adair <tryndaadair@gmail.com>

--- base_custom_plugin.php
+++ base_custom_plugin.php
@@ -1,14 +1,28 @@
 <?php
-/*
-	Plugin Name: [Plugin Name]
-	Creation Date: DD/MM/YYYY
-	Author: [Plugin Author]
-	Description: This is a plugin designed to [Do Something] for the [Domain Name] website.
-	Version: 1.0
-	
-	Last Updated: [Current Date - DD/MM/YYYY]
-	Updated By: [Update Author]
-*/
+/**
+ * [Plugin Name]
+ *
+ * @package           [PluginPackage]
+ * @author            [Author Name]
+ * @copyright         [Year] [Author Name or Company Name]
+ * @license           [License Identifier]
+ *
+ * @wordpress-plugin
+ * Plugin Name:       [Plugin Name]
+ * Plugin URI:        [https://example.com/plugin-name]
+ * Description:       [Description of the plugin.]
+ * Version:           [1.0.0]
+ * Requires at least: [5.2]
+ * Requires PHP:      [7.2]
+ * Author:            [Author Name]
+ * Author URI:        [https://example.com]
+ * Text Domain:       [plugin-slug]
+ * License:           [GPL v2 or later]
+ * License URI:       [http://www.gnu.org/licenses/gpl-2.0.txt]
+ * Update URI:        [https://example.com/my-plugin/]
+ * Requires Plugins:  [my-plugin, yet-another-plugin]
+ *
+ */
 	
 	defined('ABSPATH') || exit;
 	
